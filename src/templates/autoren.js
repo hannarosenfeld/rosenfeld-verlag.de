5a3a413b0fd860bbe2b0a0c1ef0d0b86f8fc4f68
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-11 20:04:08 -0500

    Autoren Page Bootstrap Cards.

--- src/templates/autoren.js
+++ src/templates/autoren.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
-
+import Card from 'react-bootstrap/Card'
 import Layout from '../components/Layout.js'
 import './autoren.css'
 
 const AutorenPage = props => {
   props.data.allDatoCmsAutor.nodes.map(autor => {
-    console.log(autor.name)
+    console.log(autor.autorenBild.gatsbyImageData.images.fallback.src)
   })
 
   return(
     <Layout>
+      <div className="d-flex flex-wrap">
       {props.data.allDatoCmsAutor.nodes.map(autor => {
         return(
-          <div className="autoren-container">
-          <div className="autor-container" key={autor.originalId}>
-            <h5>{autor.name}</h5>
-            <p>{autor.beruf}</p>
-            <p>{autor.biographie}</p>
-            <Link >...weiterlesen</Link>
-          </div>
-          </div>
+          <Card style={{ width: '18rem' }} key={autor.originalId}>
+            <Card.Body>
+              <Card.Title>{autor.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{autor.beruf}</Card.Subtitle>
+              <Card.Img src={autor.autorenBild.gatsbyImageData.images.fallback.src} />
+              <Card.Text>{autor.biographie}</Card.Text>
+              <Card.Link href="#">...weiterlesen</Card.Link>
+            </Card.Body>
+          </Card>
         )
       })}
+      </div>
     </Layout>
   )
 }
@@ -37,6 +40,9 @@ export const query = graphql`
         name
         biographie
         beruf
+        autorenBild {
+          gatsbyImageData
+        }
       }
     }
   }
