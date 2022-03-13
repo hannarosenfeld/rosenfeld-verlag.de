d50f631fb82826761620ee82e782b4ca0dd8e5c2
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-12 19:14:25 -0500

    Try to solve the firefox bug.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,9 @@ const IndexPage = () => {
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen />
+          allowFullScreen
+          xFrameOptions="SAMEORIGIN"
+        />
       </div>
       <div className="section">
         <div className="section-inner">
