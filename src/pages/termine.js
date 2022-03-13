cfcc362d8ea1a71ea409c6be5a329c16f0fe65ef
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-12 21:24:24 -0500

    Add Baustelle to Termine.

---
+++ src/pages/termine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TerminePage = () => (
+  <div style={{margin: "2em auto", display: "flex", alignItems: "center"}}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Baustelle.svg/1200px-Baustelle.svg.png" style={{width:"50%", margin: "0 auto"}}/>
+  </div>
+)
+
+export default TerminePage
