35bb1f5e9840d24f28c70c95f6b036361c51cb87
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-13 12:30:17 -0400

    Mobile styling index page.

--- src/pages/verlag.js
+++ src/pages/verlag.js
@@ -1,32 +1,33 @@
 import React from 'react'
 import Layout from '../components/Layout.js'
 
+import './verlag.css'
 import theo from '../images/rosenfeld.jpg'
 
 
 const VerlagPage = () => (
   <Layout>
     <main className="section">
-    <div className="d-flex">
-      <div>
-        <img src={theo} className="mb-2" />
-        <p>Theo Rosenfeld</p>
+      <div className="verlagpage-inner d-flex">
+        <div className="container-image-theo">
+          <img src={theo} className="mb-2 " />
+          <p>Theo Rosenfeld</p>
+        </div>
+        <div  className="verlag-text" style={{padding: "0 2em"}}>
+          <h2 className="mb-4">Der Rosenfeld Verlag</h2>
+          <div>
+            <p>
+              1998 aus der Idee heraus gegründet, außergewöhnliche Autoren und Fotografen, die am Anfang ihrer Laufbahn standen, zu verlegen und zu fördern, hatte schon das erste Buch des Rosenfeld Verlages einen großen Erfolg: „Ausflug auf der Vjatka“ brachte der Fotografin Annette Hornischer den Kodak Fotobuchpreis und Alexander Ikonnikov, dem Autor der Essays über das Alltagsleben in russischen Dörfern, die Aufmerksamkeit etablierter Buchverlage ein.
+            </p>
+            <p>
+              Nun setzt der Rosenfeld Verlag seine Arbeit mit der gleichen Begeisterung für die Werke jener Autoren fort, die uns eine unbekannte innere und äußere Lebenswelt näher bringen.
+            </p>
+            <p>
+              Der Verleger Theo Rosenfeld, geboren 1966, ist Kopf und Herz des Verlags und in beiden Welten, in Bilder- und Sprachwelt gleichermaßen, als Texter, Designer und Fotograf seit über dreißig Jahren zu Hause.
+            </p>
+          </div>
+        </div>
       </div>
-      <div style={{padding: "0 2em"}}>
-      <h2 className="mb-4">Der Rosenfeld Verlag</h2>
-      <div>
-        <p>
-          1998 aus der Idee heraus gegründet, außergewöhnliche Autoren und Fotografen, die am Anfang ihrer Laufbahn standen, zu verlegen und zu fördern, hatte schon das erste Buch des Rosenfeld Verlages einen großen Erfolg: „Ausflug auf der Vjatka“ brachte der Fotografin Annette Hornischer den Kodak Fotobuchpreis und Alexander Ikonnikov, dem Autor der Essays über das Alltagsleben in russischen Dörfern, die Aufmerksamkeit etablierter Buchverlage ein.
-        </p>
-        <p>
-          Nun setzt der Rosenfeld Verlag seine Arbeit mit der gleichen Begeisterung für die Werke jener Autoren fort, die uns eine unbekannte innere und äußere Lebenswelt näher bringen.
-        </p>
-        <p>
-          Der Verleger Theo Rosenfeld, geboren 1966, ist Kopf und Herz des Verlags und in beiden Welten, in Bilder- und Sprachwelt gleichermaßen, als Texter, Designer und Fotograf seit über dreißig Jahren zu Hause.
-        </p>
-      </div>
-      </div>
-    </div>
     </main>
   </Layout>
 )
