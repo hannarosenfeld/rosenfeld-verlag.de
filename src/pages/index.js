8d0d72684e9eb177f21689a9bd26f557e6df1fd6
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-12 19:27:48 -0500

    Replace iframe with video tag in hope it will be allowed to play now.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,18 @@ import ShowCase from '../components/ShowCaseBuch.js'
 const IndexPage = () => {
   return (
     <Layout>
-      <div className="mt-5">
-        <iframe
-          width="100%"
-          height="500px"
-          src={lesung}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          xFrameOptions="SAMEORIGIN"
-        />
-      </div>
-      <div className="section">
-        <div className="section-inner">
-          <h2>Was wir über die Welt wissen, das wissen wir vor allem über Bilder und über Texte von Autorinnen und Autoren, die uns ein Fenster zu einem anderen Ort öffnen. Der Rosenfeld Verlag setzt nun nach über 20 Jahren sein Programm fort und begibt sich wieder auf Reisen zu inneren und äußeren Orten, die unser Leben bereichern werden.</h2>
+      <main>
+        <div className="section">
+          <video controls width="100%">
+            <source src={lesung} type="video/mp4" />
+          </video>
         </div>
-      </div>
+        <div className="section">
+          <div className="section-inner">
+            <h2>Was wir über die Welt wissen, das wissen wir vor allem über Bilder und über Texte von Autorinnen und Autoren, die uns ein Fenster zu einem anderen Ort öffnen. Der Rosenfeld Verlag setzt nun nach über 20 Jahren sein Programm fort und begibt sich wieder auf Reisen zu inneren und äußeren Orten, die unser Leben bereichern werden.</h2>
+          </div>
+        </div>
+      </main>
       <ShowCase/>
     </Layout>
   )
