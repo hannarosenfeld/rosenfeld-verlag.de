9fb267140f4fc247c49996374fd24f06350889b7
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-24 21:46:01 -0400

    Styling Autor detail page.

--- src/templates/autor.js
+++ src/templates/autor.js
@@ -10,9 +10,14 @@ export const query = graphql`
         slug
         id
         name
+        beruf
+        bio {
+          bio
+        }
         image {
           url
          }
+        fotoCredit
       }
     }
 `
@@ -23,10 +28,17 @@ const Autor = props => {
     const autor = props.data.contentfulAuthor
     return (
         <Layout>
-            <div className="autor-detail-container d-flex">
-                <h1>{autor.name}</h1>
+            <div className="autor-detail-container d-flex mt-5 mb-5">
                 <div className="autor-detail-image-container">
                     <img src={autor.image.url}/>
+                    <p>Foto: {autor.fotoCredit}</p>
+                </div>
+                <div style={{width: "70%", margin: "0 6em"}}>
+                    <h1>{autor.name}</h1>
+                    <p>{autor.beruf}</p>
+                    <div className="mt-5" style={{width: "80%"}}>
+                        {autor.bio.bio}
+                    </div>
                 </div>
             </div>
         </Layout>
