6ec32f4f4011209d89fda1cb7e85821af5e62b1d
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-26 14:31:26 -0400

    Source Book data from Contentful.

--- src/pages/buecher.js
+++ src/pages/buecher.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import { graphql} from 'gatsby'
+
 import Layout from '../components/Layout.js'
 
 
-const BooksPage = () => (
+export const query = graphql`
+  query BuecherPageQuery{
+    allContentfulBook {
+      nodes {
+        id
+        title
+         author {
+           beruf
+           name
+           id
+         }
+       }
+     }
+   }
+`
+
+
+const BooksPage = props => {
+    return(
   <Layout>
-    <div style={{margin: "2em auto", display: "flex", alignItems: "center"}}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Baustelle.svg/1200px-Baustelle.svg.png" style={{width:"50%", margin: "0 auto"}}/>
+      <div style={{margin: "2em auto", display: "flex", alignItems: "center"}}>
+          {props.data.allContentfulBook.nodes.map(book => {
+              return(
+                  <h1>{ book.title }</h1>
+              )
+          })}
     </div>
   </Layout>
-)
+    )}
 
 export default BooksPage
