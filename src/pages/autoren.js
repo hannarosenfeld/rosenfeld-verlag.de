69d0f71440140f1e4ac8ca98199a6fc07747a09b
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-27 11:42:23 -0400

    Uniform sizing of images.

--- src/pages/autoren.js
+++ src/pages/autoren.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout.js'
 import '../styles/autoren.css'
 
 const AutorenPage = props => {
+    console.log(props)
     return(
         <Layout>
             <main className="d-flex flex-column" >
@@ -21,7 +22,7 @@ const AutorenPage = props => {
                                         </div>
                                         <div className="ipad-flex">
                                             <div className="ipad-author-picture">
-                                                <Card.Img className="mb-3" src={autor.image.url} alt={autor.name}/>
+                                                <Card.Img className="autor-image mb-3" src={autor.image.url} alt={autor.name}/>
                                                 <Card.Subtitle className="mb-3">{autor.fotoCredit}</Card.Subtitle>
                                             </div>
                                             <div className="ipad-author-bio">
