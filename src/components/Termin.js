d7d38bbb6a9be9ba0cec62f81f39cace849c781c
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-17 17:39:01 -0400

    Add Termin Page.

---
+++ src/components/Termin.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+
+export default function Termin() {
+    const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      allContentfulTermin {
+        nodes {
+          title
+          ort
+          datum
+          link
+          autor
+          image {
+            gatsbyImageData(width: 200)
+          }
+        }
+      }
+    }
+  `)
+    return (
+        <>
+          {data.allContentfulTermin.nodes.map(termin => {
+              const image = getImage(termin.image)
+              return(
+                  <div style={{width: "100%", background: "#D3D8C3"}}>
+                    <div className="d-flex justify-content-between m-3">
+                      <div className="d-flex" style={{gap: "1em"}}>
+                        <GatsbyImage image={image}/>
+                        <h3>{termin.autor}</h3>
+                      </div>
+                      <div>
+                        <div className="d-flex" style={{gap: "1em"}}>
+                          <h3>{termin.datum} / {termin.title}</h3>
+                        </div>
+                        <h3><a href={termin.link}>{termin.ort}</a></h3>
+                        <h3></h3>
+                      </div>
+                    </div>
+                  </div>
+              )
+          })}
+        </>
+    )
+}
--- src/pages/termine.js
+++ src/pages/termine.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout.js'
-
+import Termin from '../components/Termin'
 
 const TerminePage = () => (
   <Layout>
   <div style={{margin: "2em auto", display: "flex", alignItems: "center"}}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Baustelle.svg/1200px-Baustelle.svg.png" style={{width:"50%", margin: "0 auto"}}/>
+    <Termin/>
   </div>
   </Layout>
 )
