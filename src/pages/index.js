b298876839e56edf86e1f247dfbe12d03db8bc10
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-22 11:08:11 -0400

    Index Page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,16 @@ function IndexPage() {
                 </p>
               </div>
             </div>
+            <VideoBanner/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <Vjatka/>
+            <br/>
+            <br/>
+            <br/>
             <Termin/>
             <Newsletter/>
             <Footer/>
@@ -46,57 +55,57 @@ function IndexPage() {
 
 export default IndexPage
 
-// function VideoBanner(){
-//     const [isShown, setIsShown] = useState(false);
-//     function handleClick(e) {
-//         var video = document.querySelector("video")
+function VideoBanner(){
+    const [isShown, setIsShown] = useState(false);
+    function handleClick(e) {
+        var video = document.querySelector("video")
 
-//         if(isShown === true) {
-//             //          video.setAttribute("muted", "false")
-//             video.muted = !video.muted;
-//             document.getElementById("video-modal").style.display = "none"
-//         } else {
-//             console.log("hi")
-//         }
-//     }
-//     return(
-//         <div
-//           className="video-banner"
-//           style={{
-//               width: "100%",
-//               position: "relative",
-//               zIndex: "0",
-//               display: "flex",
-//           }}
-//           onMouseEnter={() => setIsShown(true)}
-//           onMouseLeave={() => setIsShown(false)}
-//         >
-//           {isShown && document.querySelector("video").muted === true && (
-//               <div
-//                 id="video-modal"
-//                 style={{
-//                     fontSize: "1.8em",
-//                     margin: "0",
-//                     top: "50%",
-//                     left: "50%",
-//                     marginRight: "-50%",
-//                     transform: "translate(-50%, -50%)",
-//                     padding: "1.8em",
-//                     zIndex: "10",
-//                     position: "absolute",
-//                     display: "block",
-//                     color: "#f9f9f9",
-//                     background: "rgba(33,33,33, 0.8)",
-//                     borderRadius: "0.23em"
-//                 }}
-//                 onClick={handleClick}
-//               >
-//                 Ton einschalten mit Klick
-//               </div>
-//           )}
-//           <video autoPlay muted id="homepage-video" className="video-section" style={{width: "100%"}}>
-//             <source src={lesung} type="video/mp4" />
-//           </video>
-//         </div>
-//     )
-// }
+        if(isShown === true) {
+                     video.setAttribute("muted", "false")
+            video.muted = !video.muted;
+            document.getElementById("video-modal").style.display = "none"
+        } else {
+            console.log("hi")
+        }
+    }
+    return(
+        <div
+          className="video-banner"
+          style={{
+              width: "100%",
+              position: "relative",
+              zIndex: "0",
+              display: "flex",
+          }}
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
+        >
+          {isShown && document.querySelector("video").muted === true && (
+              <div
+                id="video-modal"
+                style={{
+                    fontSize: "1.8em",
+                    margin: "0",
+                    top: "50%",
+                    left: "50%",
+                    marginRight: "-50%",
+                    transform: "translate(-50%, -50%)",
+                    padding: "1.8em",
+                    zIndex: "10",
+                    position: "absolute",
+                    display: "block",
+                    color: "#f9f9f9",
+                    background: "rgba(33,33,33, 0.8)",
+                    borderRadius: "0.23em"
+                }}
+                onClick={handleClick}
+              >
+                Ton einschalten mit Klick
+              </div>
+          )}
+          <video autoPlay muted id="homepage-video" className="video-section" style={{width: "100%"}}>
+            <source src={lesung} type="video/mp4" />
+          </video>
+        </div>
+    )
+}
