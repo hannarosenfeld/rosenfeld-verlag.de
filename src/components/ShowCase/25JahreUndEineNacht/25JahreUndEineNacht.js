6a6cf70210aa738146f295e702920d05f4d0e198
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-29 18:49:37 -0400

    Phew.

--- src/components/ShowCase/25JahreUndEineNacht/25JahreUndEineNacht.js
+++ src/components/ShowCase/25JahreUndEineNacht/25JahreUndEineNacht.js
@@ -8,25 +8,21 @@ import cover from '../../../images/45jahre.png'
 const Buch = () => (
     <section>
       <div>
-        <div className="hero-banner">
-          <div style={{zIndex: "1", gridColumn: "2/4", gridRow: "2/4"}}>
-            <div className="hero-content" className="d-flex" style={{gap: "2em"}}>
-              <img src={cover} />
-              <div className="d-flex">
-                <div className="hero-text d-flex flex-column" style={{width: "95%", gap: "1em"}}>
+    <div className="hero-banner" style={{height: "50vw"}}>
+      <div style={{zIndex: "1"}}>
+        <div className="hero-content d-flex" style={{gap: "1em", margin: "10% 5%", width: "60%"}}>
+          <img src={cover} className="hidden" style={{alignSelf: "start", width: "100%", margin: "0.5em"}}/>
+              <div className="d-flex" >
+                <div className="hero-text d-flex flex-column" style={{gap: "0.2em", margin: "0.5em"}}>
                   <h3>﻿Gernot Maria Grohs</h3>
                   <h2>﻿25 Jahre und eine Nacht</h2>
-                  <p>﻿Es ist der 45. Hochzeitstag von Ludwig
+                  <p className="truncate">﻿Es ist der 45. Hochzeitstag von Ludwig
                     und Tina. Es soll ein besonderer Tag
                     werden und deshalb werden die engsten
                     Freunde zur Feier des Jahrestages zu einem
                     Fest eingeladen. Man isst, trinkt und
-                    schwelgt in gemeinsamen Erinnerungen eines
-                    Lebens in Ostdeutschland, mit all seinen
-                    Besonderheiten, Merkwürdigkeiten und
-                    Brüchen. Als das Thema auf die Liebe, die
-                    Treue und ...</p>
-    <p className="mt-3" style={{fontWeight: "bold"}}><a href="buecher/">mehr erfahren</a></p>
+                    schwelgt in gemeinsamen Erinnerungen ...</p>
+    <p className="mt-1" style={{fontWeight: "bold"}}><a href="buecher/">mehr erfahren</a></p>
                 </div>
               </div>
             </div>
