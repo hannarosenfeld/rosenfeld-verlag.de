2d6bc014f9f2d8af6775b0163ec2f14255ab1bfe
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-29 17:30:36 -0400

    Style.

--- src/templates/autor.js
+++ src/templates/autor.js
@@ -6,6 +6,7 @@ import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer"
 
 import Layout from '../components/Layout'
 
+import "../styles/autor.css"
 
 export default function AuthorTemplate({ data }) {
     const authors = data.allContentfulAuthor.nodes
@@ -16,15 +17,15 @@ export default function AuthorTemplate({ data }) {
 
               return(
                   <div>
-                    <div className="d-flex" style={{gap: "2em"}}>
-                      <div style={{width: "30%", padding: "0 1em", margin: "0 1em"}}>
-                        <GatsbyImage image={image} className="mb-1"/>
+                    <div className="author d-flex" style={{gap: "2em"}}>
+                      <div className="author-image" style={{padding: "0 1em", margin: "0 1em"}}>
+                        <GatsbyImage image={image} className="mb-1" style={{minWidth: "200px"}}/>
                         <p style={{fontSize: "0.8em"}}>Foto: {author.fotoCredit}</p>
                       </div>
                       <div key={author.slug} className="mb-3" style={{width: "70%"}}>
                         <h1 className="mb-4" style={{fontWeight: "bold"}}>{author.name}</h1>
                         <h2 style={{marginBottom: "2em"}} >{author.beruf}</h2>
-                        <div style={{width: "85%"}}>
+                        <div className="author-bio" style={{width: "85%"}}>
                           <p className="mb-5">
                             <MDXProvider>
                               <MDXRenderer>
@@ -32,15 +33,16 @@ export default function AuthorTemplate({ data }) {
                               </MDXRenderer>
                             </MDXProvider>
                           </p>
+
                           <div>
                             <p style={{fontWeight: "bold", fontSize: "0.9em"}} className="mb-3">Im Rosenfeld Verlag erschienen:</p>
                             <div>
                               {author.book.map( b => {
                                   const cover = getImage(b.coverImage)
                                   return(
-                                      <div>
-                                        <div className="d-flex" style={{gap: "2em"}}>
-                                            <GatsbyImage image={cover}/>
+                                      <div >
+                                        <div className="d-flex author-books-container" style={{gap: "2em", marginRight: "1em"}}>
+                                          <GatsbyImage image={cover} style={{minWidth: "8em", maxWidth: "10em"}}/>
                                           <div>
                                             <h4 style={{fontWeight: "bold"}}>{b.title}</h4>
                                             <p>{b.subtitle}</p>
