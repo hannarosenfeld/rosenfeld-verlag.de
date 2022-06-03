3dc88377cea84556dccec876b64caa8eb55729eb
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-06-03 19:38:15 -0400

    Solve link problem

--- src/pages/autoren.js
+++ src/pages/autoren.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql} from 'gatsby'
 import Card from 'react-bootstrap/Card'
 import Layout from '../components/Layout.js'
 import '../styles/autoren.css'
 
 const AutorenPage = props => {
+    const [currentAuthor, setCurrentAuthor] = useState('')
+
+    const author = props.data.allContentfulAuthor.nodes
+
     return(
         <Layout>
           <main className="d-flex flex-column" >
             <h2 className="mb-5">Unsere Autorinnen und Autoren</h2>
             <div className="d-flex flex-wrap autoren-container" style={{gap: "3em"}}>
-              {props.data.allContentfulAuthor.nodes.map(autor => {
+              {author.map(autor => {
                   return(
                       <div className="d-flex flex-wrap" >
                         <Card className="mb-5" style={{ width: '15em' }} key={autor.originalId}>
@@ -32,7 +36,7 @@ const AutorenPage = props => {
                               </div>
                               <div className="ipad-author-bio">
                                 <Card.Text style={{fontSize: "0.8em"}} className="autoren-bio mb-1 ml-1">{autor.bio.bio}</Card.Text>
-                                <Card.Link  style={{fontSize: "0.8rem", fontWeight: "bold"}} className="ipad-link" href={`autoren/${autor.slug}`}>...weiterlesen</Card.Link>
+                                <Card.Link  style={{fontSize: "0.8rem", fontWeight: "bold"}} className="ipad-link" href={`/${autor.slug}`}>...weiterlesen</Card.Link>
                               </div>
                             </div>
                           </Card.Body>
